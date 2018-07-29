597f16273411576a7c2e7d291dca3776b4902b30
rei-gasis <41265292+rei-gasis@users.noreply.github.com>
2018-07-29 13:26:56 +0800

    Add files via upload

---
+++ RCGJobManager/app/build/generated/source/buildConfig/debug/rcg/app/rcgjobmanager/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package rcg.app.rcgjobmanager;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "rcg.app.rcgjobmanager";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+}
